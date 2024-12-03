ae377ad251c5a75ccd1c43f0ecde94fdd7102682
Catalina Rivera <crivera@empresstx.com>
2024-12-03 20:27:42 +0000

    Updates mainly on integration reportss

--- R/all_reports/deg_enrich_report/MAIN_DEG_ENRICH_perdrug.R
+++ R/all_reports/deg_enrich_report/MAIN_DEG_ENRICH_perdrug.R
@@ -2,24 +2,23 @@ library(rmarkdown)
 library(tidyverse)
 library(readxl)
 
-
-
-cell_line = "In_vitro"
-stimulation = "NONE"
-scinamicNum = 'A_2024_0142'
-baseDir = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/Cell_line_Experiments/A_2024_0142/Results_Test_Pipeline_241115"
+cell_line = "In_vivo"
+stimulation = "TNBS"
+scinamicNum = 'EXP000033'
+baseDir = "~/BULK-TRANSCRIPTOMICS/InVivo/EXP000033/Results_241119_Complete"
 DirDataForPipeline = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/Transcriptomics_Pipeline/data/Data_for_pipeline"
 DirPipeline = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/Transcriptomics_Pipeline/R/all_reports/deg_enrich_report"
-dataDir =  "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/Cell_line_Experiments/A_2024_0142/Results_Test_Pipeline_241115/DESEQ_NORM"
-Contrast_path = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/Transcriptomics_Pipeline/data/In_vitro/List_contrasts_K562_Mod.xlsx"
+dataDir = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/InVivo/EXP000033/Results_241115/DESEQ_NORM"
+Contrast_path = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/InVivo/EXP000033/Data/List_contrasts_Invivo.xlsx"
 Chemo_path = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/Transcriptomics_Pipeline/data/Data_for_pipeline/Target_list_241112_w_InvitroTargets_knInh.RDS"
-expr_gene_path = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/Cell_line_Experiments/A_2024_0142/Results_Test_Pipeline_241115/PRE_FILTERING/Expressed_genes.xlsx"
+expr_gene_path = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/InVivo/EXP000033/Results_241105/PRE_FILTERING/Expressed_genes.xlsx"
 Experimental_design_path = ""
-Cell_Dict_path = "/fsx/home/crivera/BULK-TRANSCRIPTOMICS/Transcriptomics_Pipeline/data/In_vitro/CellLine_Dict_Chemo.csv"
+Cell_Dict_path ="/fsx/home/crivera/BULK-TRANSCRIPTOMICS/InVivo/EXP000033/Data/CellLine_Dict_Chemo.csv"
 AdjustDeSeq = ""
-Organism = "Human"
+Organism = "Mouse"
 padj_thr_gene = 0.2
 logFC_thrs_gene = 0
+
 
 
 Name_folder_path =  file.path(baseDir, "REPORTS")
@@ -27,9 +26,9 @@ dir.create(Name_folder_path, recursive = TRUE)
 
 Metadata <-read_xlsx(file.path(dataDir, "Norm_Data.xlsx"), sheet = 'Metadata')
 drugs = unique(Metadata$Treatment)
-drugs = drugs[drugs!= 'DMSO']
+drugs = drugs[!drugs %in% c('NONE', "Vehicle-DMSO")]
 
-drugs = "rapamycin"
+
 
 for(x in drugs){
     rmarkdown::render(input = paste(DirPipeline,"DEG_ENRICH_perdrug.Rmd", sep = '/')  ,
