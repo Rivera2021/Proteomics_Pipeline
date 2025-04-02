a9417cc9e7c3e94451c25a7a026744cd253c1cc7
John Hutchinson <john@diamondage.com>
2025-04-02 14:26:48 +0000

    remove params and load from file

--- R/all_reports/main_qc_report/MAIN_QC_REPORT.R
+++ R/all_reports/main_qc_report/MAIN_QC_REPORT.R
@@ -2,38 +2,7 @@ library(rmarkdown)
 library(tidyverse)
 library(readxl)
 
-# experimental params
-scinamicNum = 'EXP000064'
-cellline = "K562.1 & THP1.1"
-Experimental_design_path = ""
-Column_match = "ID"
-Organism_type = "In_vitro" # Either In_vivo or In_vitro
-ControlName = "Vehicle-DMSO"
-Batch_variable= ""
-ExpDescription = "The protocol used for this experiment was Watchmaker"
-
-# data and metadata locations
-expDir = "/fsx/home/john/projects/empress/EXP000064"
-baseDir = file.path(expDir, "Results_250301/")
-Metrics_path = file.path(expDir, "rawdata/multiqc/star_salmon/multiqc_report_data/")
-Metadata_path = file.path(expDir, "meta/EXP000064_metadata.xlsx")
-CountM_path =  file.path(expDir, "rawdata/star_salmon/salmon.merged.gene_counts.tsv")
-SampleName_path = file.path(expDir, "meta/SampleName.csv")
-TPM_path = file.path(expDir, "rawdata/star_salmon/salmon.merged.gene_tpm.tsv")
-
-# report params
-Prev_perc = 0.2
-MEMORY_MB = 4000
-Corr_plot_features = c("Treatment","Treatment_conc", "Treatment_time_hrs", "Stimulant_used","Sample_name")
-CoarseCondition =  c("Cell_line", "Treatment","Treatment_conc", "Treatment_time_hrs", "Stimulant_used")
-PRE_FILTER = "PREV"
-QCNORM = "PRE_QCNORM"
-
-# pipeline specific params
-DirDataForPipeline = "/fsx/home/john/repos/Transcriptomics_Pipeline/data/Data_for_pipeline"
-DirPipeline =  "/fsx/home/john/repos/Transcriptomics_Pipeline/R/all_reports/main_qc_report"
-Alias_path = "/fsx/home/john/repos/Transcriptomics_Pipeline/data/Data_for_pipeline/mol_ids.csv"
-
+source("/fsx/home/john/projects/empress/EXP000090/params_qc_groupeddonors.txt")
 
 
 Name_folder_path =  file.path(baseDir, "REPORTS")
